e95f6e962cf07a4ce69af3cfdc12ad62e4029e28
omer <sirinsovalye4@gmail.com>
2025-04-27 11:09:44 +0300

    v0.4
    Add mqtt

--- esp32MQTT.ino
+++ esp32MQTT.ino
@@ -1,8 +1,13 @@
 #include <WiFi.h>
+#include <PubSubClient.h>
 #include "QMI8658.h"
 
 const char* ssid = "wifiName";
 const char* password = "wifiPassword";
+const char* mqtt_broker = "127.0.0.1";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void setup_wifi() {
   WiFi.begin(ssid, password);
@@ -15,6 +20,19 @@ void setup_wifi() {
   Serial.println("WiFi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.println("Connecting MQTT...");
+    if (client.connect("ESP32Client")) {
+      Serial.println("Connected MQTT!");
+    } else {
+      Serial.print("Error: ");
+      Serial.println(client.state());
+      delay(2000);
+    }
+  }
 }
 
 void setup() {
@@ -32,9 +50,13 @@ void setup() {
   QMI8658_init();
 
   setup_wifi();
+  client.setServer(mqtt_broker, 1883);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
 
 }
