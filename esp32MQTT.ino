eff8b411fb85109216f93b242da1b134f991e139
omer <sirinsovalye4@gmail.com>
2025-04-27 10:56:06 +0300

    v0.3
    Add wifi

--- esp32MQTT.ino
+++ esp32MQTT.ino
@@ -1,4 +1,21 @@
+#include <WiFi.h>
 #include "QMI8658.h"
+
+const char* ssid = "wifiName";
+const char* password = "wifiPassword";
+
+void setup_wifi() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
 
 void setup() {
   Serial.begin(115200);
@@ -13,6 +30,8 @@ void setup() {
     Serial.println("GPIO Init successful!");
 
   QMI8658_init();
+
+  setup_wifi();
 }
 
 void loop() {
